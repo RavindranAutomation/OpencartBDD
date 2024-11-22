1a49799f89acf0096460f2e4a6dacee600df4ecf
Ravindran Automation <ravinpauly10@gmail.com>
2024-11-22 13:27:40 +0530

    New

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/featureFiles",
+		features="src/test/resources/featureFiles/Login.feature",
 		glue= {"stepDefinitions","hooks"},
 		dryRun=!true,
 		monochrome=true,
@@ -15,8 +15,8 @@ import io.cucumber.junit.CucumberOptions;
 
 		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failedscenario.txt",
-				"html:target/Cucumber.html",
-		"json:target/Cucumber.json"}
+				"html:target/cucumber-reports/Cucumber.html",
+		"json:target/cucumber-reports/Cucumber.json"}
 		)
 
 public class TestRunner {
